727ab2180fc1c9e7d69b8d96118b84b8981e7c30
rmccloskey <rmccloskey8@gmail.com>
2016-08-20 20:43:39 +0100

    Added Initial Scene and Interaction

---
+++ Assets/Scripts/Artwork.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Artwork : MonoBehaviour
+{
+    public string title;
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+    Camera m_Camera;
+    [SerializeField]
+    GameObject m_ArtworkPanel;
+    Animator m_ArtworkPanelAnim;
+    Text m_ArtworkTitleText;
+
+    [SerializeField]
+    LayerMask m_LayerMask;
+    
+    void Start()
+    {
+        m_Camera = Camera.main;
+        m_ArtworkPanelAnim = m_ArtworkPanel.GetComponent<Animator>();
+        m_ArtworkTitleText = m_ArtworkPanel.GetComponentInChildren<Text>();
+    }
+    
+    void Update()
+    {
+        Ray ray = new Ray(m_Camera.transform.position, m_Camera.transform.forward);
+        RaycastHit hit;
+
+        if (Physics.SphereCast(ray, 1.5f, out hit, 4, m_LayerMask, QueryTriggerInteraction.UseGlobal))
+        {
+            string title = hit.collider.gameObject.GetComponent<Artwork>().title;
+            Debug.Log("Artwork " + title + " is in view");
+            if (!m_ArtworkPanelAnim.GetBool("Show"))
+            {
+                m_ArtworkTitleText.text = title;
+                m_ArtworkPanelAnim.SetBool("Show", true);
+            }
+        }
+        else
+        {
+            if (m_ArtworkPanelAnim.GetBool("Show"))
+                m_ArtworkPanelAnim.SetBool("Show", false);
+                
+        }
+    }
+}
